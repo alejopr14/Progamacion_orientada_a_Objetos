fb74e498075872573efb3f2686f0c2eb5fde3d0f
alejopr14 <alejopr14@gmail.com>
2025-08-31 17:30:40 -0500

    Delete Actividad_Condicionales/par_impar.py

--- Actividad_Condicionales/par_impar.py
+++
@@ -1,8 +0,0 @@
-numerito = int(input("Ingrese un numero: "))
-
-if numerito == 0:
-    print("El numero es cero")
-elif numerito % 2 == 0:
-    print(f"{numerito} es un numero par")
-else:
-    prin(f"{numerito} es un numero impar")
